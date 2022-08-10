9e4012c1a3915d63bb5b2d335e1e2cae2ed776bb
elifelalmis <e.elalmis-2021@hull.ac.uk>
2022-08-10 10:25:03 +0100

    removed non-components

--- connectedComponent.py
+++ connectedComponent.py
@@ -30,41 +30,38 @@ def CC(SWT, originalImage, direction = 1 ):
             
             nLabels = []
 
-            if strokeOfPixel > 0:
-                if not nolabeledNeighbours :
-                    if(leftN > 0 and getDivision(leftN,strokeOfPixel)):
-                        nLabels.append(component_map[y,x-1])
-                    if(aboveN > 0  and getDivision(aboveN,strokeOfPixel)):
-                        nLabels.append(component_map[y-1,x])
-                    if(topLeft > 0 and getDivision(topLeft,strokeOfPixel)):
-                        nLabels.append(component_map[y-1,x-1])
-                    if(topRight > 0  and getDivision(topRight,strokeOfPixel)):
-                        nLabels.append(component_map[y-1,x+1])
+            if not nolabeledNeighbours :
 
-                    ref = eq_list[min(nLabels)]
-                        
-                    for n in nLabels:
-                        if eq_list[n] > min(nLabels):
-                            eq_list[n] = ref
+                if(leftN > 0 and getDivision(leftN,strokeOfPixel)):
+                    nLabels.append(component_map[y,x-1])
+                if(aboveN > 0  and getDivision(aboveN,strokeOfPixel)):
+                    nLabels.append(component_map[y-1,x])
+                if(topLeft > 0 and getDivision(topLeft,strokeOfPixel)):
+                    nLabels.append(component_map[y-1,x-1])
+                if(topRight > 0  and getDivision(topRight,strokeOfPixel)):
+                    nLabels.append(component_map[y-1,x+1])
 
-                    component_map[y,x] = ref
+                if len(nLabels)>1:
+                    max = int(np.max(nLabels))
+                    min = int(np.min(nLabels))
+                    component_map[y,x] = min
+                    eq_list[max] = min
+                else:
+                    component_map[y,x] = np.min(nLabels)
 
-                    components[ref].append([y,x])
+            elif nolabeledNeighbours:
+                
+                label += 1
 
-                elif nolabeledNeighbours:
-                    
-                    label += 1
-                    eq_list[label] = label
-                    component_map[y,x] = label
+                eq_list[label] = label
+                component_map[y,x] = label
 
-                    if hasattr(components, str(label)):
-                        components[label].append([y,x])
-                    else:
-                        components[label] = []
-                        components[label].append([y,x])
-            else:
-                component_map[y,x] = 0
-        
+                if hasattr(components, str(label)):
+                    components[label].append([y,x])
+                else:
+                    components[label] = []
+                    components[label].append([y,x])
+
         except IndexError:
             continue
     
@@ -74,76 +71,18 @@ def CC(SWT, originalImage, direction = 1 ):
         x = componentPointsCol[index]
 
         try:
-            if component_map[y,x] > 0:
-                val = component_map[y,x]
-                if component_map[y,x] > eq_list[val]:
-                    component_map[y,x] = eq_list[val]
+            currentLabel = component_map[y,x]
+            newLabel = eq_list[currentLabel]
 
-                components[eq_list[val]].append([y,x])
+            component_map[y,x] = newLabel
+            
+
+            components[newLabel].append([y,x])
             
         except IndexError:
             continue
 
-    newResult  =  np.zeros(component_map.shape)
-
-    for component in list(components):
-        '''calculate variance rate'''
-        arrS = [SWT[p[0], p[1]] for p in components[component]]
-        
-        arrY = sorted(components[component], key=lambda x: x[0])
-        arrX = sorted(components[component], key=lambda x: x[1])
-
-        minY  =  arrY[0][0]
-        maxY  =  arrY[len(arrY) - 1 ][0]
-        minX  =  arrX[0][1]
-        maxX  =  arrX[len(arrX) - 1 ][1]
-
-        height = maxY - minY + 1
-        width = maxX - minX + 1
-
-        aspect = width  / height
-
-        varianceV = np.var(arrS)
-        averageV = np.mean(arrS)
-        
-        median = np.median(arrS)
-        diameter = np.sqrt( height ** 2 + width ** 2)
-        uniqueArr = np.unique(component_map[minY:minY+height, minX:minX+width])
-
-        '''CONDITIONS'''
-        varianceRatio = (varianceV/averageV) <= 2 
-        aspectRatio = 0.1 <= aspect <= 10
-        diameterRatio = (median / diameter) <= 15.00
-        heightV = 10 <= height <= 300
-        widthV =  10 <= width <= 300
-        compCount = len(uniqueArr[uniqueArr != 0]) <=3
+    components = {k: v for k, v in components.items() if len(v) > 1}
 
 
-        if varianceRatio and aspectRatio and diameterRatio and heightV and widthV and compCount:
-            #result = cv2.rectangle(result, (minX,minY ), (minX + width ,minY+height), (255,0,255), 1)
-            for p in components[eq_list[component]]:
-                newResult[p[0],p[1]] = eq_list[component]
-
-            arrY = sorted(components[eq_list[component]], key=lambda x: x[0])
-            arrX = sorted(components[eq_list[component]], key=lambda x: x[1])
-
-            minY  =  arrY[0][0]
-            maxY  =  arrY[len(arrY) - 1 ][0]
-            minX  =  arrX[0][1]
-            maxX  =  arrX[len(arrX) - 1 ][1]
-
-            height = maxY - minY + 1
-            width = maxX - minX + 1
-
-            comp = originalImage[minY:minY+height, minX:minX+width]
-        
-            avg_color_per_row = np.average(comp[newResult[minY:minY+height, minX:minX+width]> 0],axis=0)
-
-            avg_color = np.average(avg_color_per_row, axis=0)
-
-            componentFeatureList[eq_list[component]] = [median, height, minX, maxX, avg_color]
-        
-        
-
-                
-    return newResult, components, componentFeatureList 
+    return component_map,components
