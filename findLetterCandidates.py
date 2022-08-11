74549abe71e31358d0fc1dfe3bf6776211afaa0d
vrlelif <57722793+vrlelif@users.noreply.github.com>
2022-08-11 16:52:47 +0100

    Create findLetterCandidates.py

---
+++ findLetterCandidates.py
@@ -0,0 +1,76 @@
+from matplotlib import pyplot as plt
+from StrokeWidthTransform import SWT_apply
+from connectedComponent import CC
+from readImage import Read
+import timeit
+import numpy as np
+
+def findLetterCandidates(component_map, components , SWTResult, originalImage):
+    newResult  =  np.zeros(component_map.shape)
+    features = {}
+
+    for component in list(components):
+
+        c = np.unique(components[component], axis=0)
+
+        arrS = [SWTResult[p[0], p[1]] for p in c]
+
+        height = max(c[:,0]) - min(c[:,0]) + 1
+        width =  max(c[:,1]) - min(c[:,1]) + 1
+
+        aspect = width  / height
+
+        varianceV = np.var(arrS)
+        averageV = np.mean(arrS)
+        
+        median = np.median(arrS)
+        diameter = np.sqrt( height ** 2 + width ** 2)
+        componentArea = component_map[ min(c[:,0]): min(c[:,0]) + height, min(c[:,1]):min(c[:,1] ) + width]
+        
+        uniqueArr = np.unique(componentArea)
+
+        '''CONDITIONS'''
+        varianceRatio = (varianceV/averageV) <= 2 
+        aspectRatio = 0.1 <= aspect <= 10
+        diameterRatio = (median / diameter) <= 15.00
+        heightV = 10 <= height <= 300
+        widthV =  10 <= width <= 300
+        compCount = len(uniqueArr[uniqueArr != 0]) <=3
+
+        if not varianceRatio:
+            continue
+        if not aspectRatio:
+            continue
+        if not diameterRatio:
+            continue  
+        if not heightV:
+            continue
+        if not widthV:
+            continue
+        if not compCount:
+            continue
+
+        for p in components[component]:
+            newResult[p[0],p[1]] = component
+
+    
+        originalImageSliceNoNZero = originalImage[ min(c[:,0]): min(c[:,0]) + height, min(c[:,1]):min(c[:,1] ) + width]
+        componentMapSlice = newResult[ min(c[:,0]): min(c[:,0]) + height, min(c[:,1]):min(c[:,1] ) + width]
+        #avg_color_per_row_BEFORE = np.average(originalImageSliceNoNZero,axis=0)
+        originalImageSliceNoNZero = originalImageSliceNoNZero[componentMapSlice > 0 ]
+
+        avg_color_per_row_AFTER = np.average(originalImageSliceNoNZero,axis=0)
+
+
+        features[component] = {
+            'medianS' : median,
+            'height' : height,
+            'width' : width,
+            'maxX' :  max(c[:,1]),
+            'minX' : min(c[:,1]),
+            'avgColor' : avg_color_per_row_AFTER
+        }
+
+                
+    return newResult, features
+
