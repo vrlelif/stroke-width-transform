3c3bff4140922b3dba25716c950f32c4a3d8e0e2
vrlelif <57722793+vrlelif@users.noreply.github.com>
2022-08-14 16:32:27 +0100

    Create groupingLetters.py

---
+++ groupingLetters.py
@@ -0,0 +1,51 @@
+from matplotlib import pyplot as plt
+from StrokeWidthTransform import SWT_apply
+from connectedComponent import CC
+from readImage import Read
+import timeit
+import numpy as np
+
+
+def findDistance(co1,co2):
+    widerCompWidth = min(co1['width'],co2['width'])
+
+    if co1['minY'] >= co2['maxY'] or co2['minY'] >= co1['maxY']:
+        return False
+    else:
+        dist = np.sqrt((co2['maxY'] - co1['maxY']) ** 2 + (co2['minX'] - co1['maxX']) ** 2)
+        return dist <= widerCompWidth * 3
+
+def findPairs(components):
+    pairs = []
+    for co in components:
+        for can in components:
+            if co == can:
+                continue
+            strokeM = components[co]['medianS'] / components[can]['medianS']  <= 2
+            heights = components[co]['height'] / components[can]['height']  <= 2
+            distance =  findDistance(components[co],components[can])
+            colors = np.mean(components[co]['avgColor']) / np.mean(components[can]['avgColor']) <= 3 
+            if (strokeM and heights and distance and colors):
+                pairs.append([co,can])
+
+    return pairs
+
+def groupG(pairs):
+    lenBefore = len(pairs)
+    groups = []
+    if len(pairs) > 1:
+        for i,pair in enumerate(pairs):
+            try:
+                if (any(point in pairs[i+1] for point in pair)):
+                    group = np.concatenate(( pair,pairs[i+1]))
+                    group = np.unique(group)
+                    groups.append(group)
+            except IndexError:
+                continue
+            
+    if len(groups) == 0 :
+        groupsFiltered = np.array([row for row in pairs if len(row)>=3])
+        return groupsFiltered
+    else: 
+        return groupG(groups)
+
